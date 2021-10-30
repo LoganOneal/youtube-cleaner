54e3ffb195e3334d12f20249db24b9a0561cd1bb
Zavier Miller <zavier@resaleai.com>
2021-10-30 18:04:09 -0400

    fix other merge err

--- jobs/filtering_job.py
+++ jobs/filtering_job.py
@@ -8,68 +8,63 @@ BUCKET_NAME = "youtube-cleaner"
 ASSEMBLY_API_KEY = "4951dd81c6854670a21a59696359e7ac"
 
 @rq.job
-<<<<<<< HEAD
 class FilteringJob:
   upload_url = ""
   download_url = ""
 
   def __call__(self, video_url):
-    return
-=======
-def FilteringJob(video_url):
-  path = "/data/"
-  fname = str(int(time.time()))
-  #download video from youtube
-  yt=YouTube(video_url)
-  stream = yt.streams.get_by_itag(22)
-  stream.download(output_path=path, filename=fname + ".mp4")
+    path = "/data/"
+    fname = str(int(time.time()))
+    #download video from youtube
+    yt=YouTube(video_url)
+    stream = yt.streams.get_by_itag(22)
+    stream.download(output_path=path, filename=fname + ".mp4")
 
-  #convert video to mp3
-  mp4_file = fname + ".mp4"
-  mp3_file = fname + ".mp3"
-  os.system("ffmpeg -i {} {}".format(mp4_file, mp3_file))
+    #convert video to mp3
+    mp4_file = fname + ".mp4"
+    mp3_file = fname + ".mp3"
+    os.system("ffmpeg -i {} {}".format(mp4_file, mp3_file))
 
-  #upload mp3 to google bucket
-  storage_client = storage.Client.from_service_account_json('/app/keys/service-account.json')
-  
-  bucket = storage_client.bucket(BUCKET_NAME)
-  blob = bucket.blob(mp3_file)
-  blob.upload_from_filename(path + mp3_file)
-
-  print(
-      "File {} uploaded to {}.".format(
-          source_file_name, destination_blob_name
-      )
-  )
+    #upload mp3 to google bucket
+    storage_client = storage.Client.from_service_account_json('/app/keys/service-account.json')
     
-  bucket_url = blob.generate_signed_url(
-  version="v4",
-  # This URL is valid for 15 minutes
-  expiration=datetime.timedelta(minutes=15),
-      # Allow PUT requests using this URL.
-      method="GET",
-  )
-  
-  # upload to assemblyAI
-  endpoint = "https://api.assemblyai.com/v2/transcript"
-  json = {
-  "audio_url": bucket_url
-  }
-  headers = {
+    bucket = storage_client.bucket(BUCKET_NAME)
+    blob = bucket.blob(mp3_file)
+    blob.upload_from_filename(path + mp3_file)
+
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, destination_blob_name
+        )
+    )
+      
+    bucket_url = blob.generate_signed_url(
+    version="v4",
+    # This URL is valid for 15 minutes
+    expiration=datetime.timedelta(minutes=15),
+        # Allow PUT requests using this URL.
+        method="GET",
+    )
+    
+    # upload to assemblyAI
+    endpoint = "https://api.assemblyai.com/v2/transcript"
+    json = {
+    "audio_url": bucket_url
+    }
+    headers = {
+        "authorization": ASSEMBLY_API_KEY,
+        "content-type": "application/json"
+    }
+    response = requests.post(endpoint, json=json, headers=headers)
+    assembly_id = response.json()['id']
+    
+    #transribe audio on AssmeblyAI
+    endpoint = "https://api.assemblyai.com/v2/transcript/" + assembly_id
+    headers = {
       "authorization": ASSEMBLY_API_KEY,
-      "content-type": "application/json"
-  }
-  response = requests.post(endpoint, json=json, headers=headers)
-  assembly_id = response.json()['id']
-  
-  #transribe audio on AssmeblyAI
-  endpoint = "https://api.assemblyai.com/v2/transcript/" + assembly_id
-  headers = {
-    "authorization": ASSEMBLY_API_KEY,
-  }
-  response = requests.get(endpoint, headers=headers)
-  while(response.json()["status"] != "completed" and response.json()["status"] != "error"):
-    time.sleep(1)
+    }
     response = requests.get(endpoint, headers=headers)
-  return response.json()
->>>>>>> 7379eda480e8f2f4f0048f2fbfdb99d457452f46
+    while(response.json()["status"] != "completed" and response.json()["status"] != "error"):
+      time.sleep(1)
+      response = requests.get(endpoint, headers=headers)
+    return response.json()
